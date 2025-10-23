d4b85af70ff375183651ed0858238ed3ef90c2cc
“bharath669” <vvbharath2006@gmail.com>
2025-10-23 13:40:18 +0100

    Role based Authentication with Spring Security

--- src/main/java/com/example/Securitydemo/GreetingsController.java
+++ src/main/java/com/example/Securitydemo/GreetingsController.java
@@ -1,5 +1,6 @@
 package com.example.Securitydemo;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +12,15 @@ public class GreetingsController {
     @GetMapping("/hello")
     public String sayHello(){
         return "Hello";
+    }
+    @PreAuthorize("hasRole('USER')")
+    @GetMapping("/user")
+    public String userEndpoint(){
+        return "Hello user!";
+    }
+
+    @GetMapping("/admin")
+    public String adminEndpoint(){
+        return "Hello admin!";
     }
 }
