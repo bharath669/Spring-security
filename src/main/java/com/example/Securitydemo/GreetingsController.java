e89566c2f3a150727aa1e2beaa7e04b73bcaf055
“bharath669” <vvbharath2006@gmail.com>
2025-10-23 10:59:56 +0100

    Basic Authentication stateless with spring security

--- src/main/java/com/example/Securitydemo/GreetingsController.java
+++ src/main/java/com/example/Securitydemo/GreetingsController.java
@@ -3,6 +3,8 @@ package com.example.Securitydemo;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.File;
+
 @RestController
 public class GreetingsController {
 
