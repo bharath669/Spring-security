6f2dfc2069779314df387288b9d63b2acfa79cb8
“bharath669” <vvbharath2006@gmail.com>
2025-10-24 19:03:30 +0100

    Creating Custom filter for Jwt Authentication with spring security with Autowired

--- src/main/java/com/example/Securitydemo/jwt/AuthTokenFilter.java
+++ src/main/java/com/example/Securitydemo/jwt/AuthTokenFilter.java
@@ -6,6 +6,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -15,8 +16,9 @@ import org.springframework.web.filter.OncePerRequestFilter;
 import java.io.IOException;
 
 public class AuthTokenFilter extends OncePerRequestFilter {
+    @Autowired
     private JwtUtils jwtUtils;
-
+    @Autowired
     private UserDetailsService userDetailsService;
 
     private static final Logger logger= LoggerFactory.getLogger(AuthTokenFilter.class);
