8b712b498da6441feb2a73e52784ef0f769fba5a
“bharath669” <vvbharath2006@gmail.com>
2025-10-24 18:58:44 +0100

    Creating Custom filter for Jwt Authentication with spring security

---
+++ src/main/java/com/example/Securitydemo/jwt/AuthTokenFilter.java
@@ -0,0 +1,51 @@
+package com.example.Securitydemo.jwt;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+public class AuthTokenFilter extends OncePerRequestFilter {
+    private JwtUtils jwtUtils;
+
+    private UserDetailsService userDetailsService;
+
+    private static final Logger logger= LoggerFactory.getLogger(AuthTokenFilter.class);
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+        try {
+            String jwt = parseJwt(request);
+            if(jwt!=null && jwtUtils.validateJwtToken(jwt)){
+                String username=jwtUtils.gettingUsernameFromJWTToken(jwt);
+                UserDetails userDetails=userDetailsService.loadUserByUsername(username);
+                UsernamePasswordAuthenticationToken authentication=
+                        new UsernamePasswordAuthenticationToken(
+                                userDetails,null,userDetails.getAuthorities()
+                        );
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+                logger.debug("Roles for jwt:{}",userDetails.getAuthorities());
+            }
+        }catch (Exception e){
+            logger.error("Cannot Set the Authentication:{}",e);
+        }
+        filterChain.doFilter(request,response);
+    }
+
+    private String parseJwt(HttpServletRequest request) {
+        String jwt=jwtUtils.getJwtFromHeader(request);
+        logger.debug("AuthTokenFilter.java:{}",jwt);
+        return jwt;
+    }
+}
